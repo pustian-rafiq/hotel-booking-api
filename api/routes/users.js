180a7c5350dad73b67a4dc1b720df50927de460a
pustian-rafiq <rafiqul.pust.cse@gmail.com>
2022-06-14 18:09:51 +0600

    verify user and admin using jwt token

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,19 +1,23 @@
 import express from 'express'
 import { deleteUserController, getUserController, getUsersController, updateUserController } from '../controllers/userController.js';
+import { verifyAdmin, verifyToken, verifyUser } from '../utils/verifyToken.js';
 
 const router = express.Router();
 
+router.get("/check",verifyToken,(req,res,next) => {
+    res.send("you are logged in")
+})
 //Update
-router.put("/:id", updateUserController)
+router.put("/:id",verifyUser, updateUserController)
 
 //Delete
-router.delete("/:id", deleteUserController)
+router.delete("/:id",verifyUser, deleteUserController)
 
 //Get Single
 
-router.get("/:id", getUserController)
+router.get("/:id",verifyUser, getUserController)
 
 //Get All
-router.get("/", getUsersController)
+router.get("/", verifyAdmin,  getUsersController)
 
 export default router
