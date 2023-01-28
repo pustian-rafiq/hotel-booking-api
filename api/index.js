529c68f4d54624828d63649799f9dc3239fbda9c
pustian-rafiq <rafiqul.pust.cse@gmail.com>
2023-01-28 21:34:05 +0600

    Modified user auth

--- api/index.js
+++ api/index.js
@@ -1,38 +1,46 @@
-import express  from "express";
-import dotenv from 'dotenv'
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
 import mongoose from "mongoose";
-import cookieParser from 'cookie-parser'
+// const cors = require("cors");
 
 //import routes here
-import authRoutes from "./routes/auth.js"
-import usersRoutes from "./routes/users.js"
-import hotelsRoutes from "./routes/hotels.js"
-import roomsRoutes from "./routes/rooms.js"
+import morgan from "morgan";
+import authRoutes from "./routes/auth.js";
+import hotelsRoutes from "./routes/hotels.js";
+import roomsRoutes from "./routes/rooms.js";
+import usersRoutes from "./routes/users.js";
 
 const app = express();
 dotenv.config();
 
 //Connect to database
 const connect = async () => {
-    try{
-        await mongoose.connect(process.env.MONGODB_URL);
-        console.log("Connect to mongodb.")
-    }catch(error){
-        throw error;
-    } 
-}
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log("Connect to mongodb.");
+  } catch (error) {
+    throw error;
+  }
+};
 
 //Event listener
-mongoose.connection.on("disconnected",()=> {
-    console.log("Mongodb disconnected");
-})
-mongoose.connection.on("connected",()=> {
-    console.log("Mongodb connected");
-})
+mongoose.connection.on("disconnected", () => {
+  console.log("Mongodb disconnected");
+});
+mongoose.connection.on("connected", () => {
+  console.log("Mongodb connected");
+});
 //Use this middleware for sending json object
-app.use(express.json())
-app.use(cookieParser())
-
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan("dev"));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 //use middleware
 app.use("/api/auth", authRoutes);
@@ -42,20 +50,19 @@ app.use("/api/rooms", roomsRoutes);
 
 //Create error middleware
 app.use((err, req, res, next) => {
-    const errorStatus = err.status || 500;
-    const errorMessage = err.message || "Something went wrong!";
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
 
-    return res.status(errorStatus).json({
-      success: false,
-      status: errorStatus,
-      message: errorMessage,
-      stack: err.stack,
-    });
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
   });
-
+});
 
 //connect to backend-run server
-app.listen(8800, ()=> {
-    connect()
-    console.log("Server is running on port 8800")
-})
+app.listen(8800, () => {
+  connect();
+  console.log("Server is running on port 8800");
+});
